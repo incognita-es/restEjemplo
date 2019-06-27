a9395022fc39c918199d718c135c546a55cc1246
incognita-es <51830325+incognita-es@users.noreply.github.com>
2019-06-27 12:31:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,29 +7,6 @@
     function buscaCurl2($direccion){
         die("HOOOOLA");
     }
-    
-    
-    function buscaCurl($direccion){
-    //next example will recieve all messages for specific conversation
-    $service_url = $direccion;
-    $curl = curl_init($service_url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $curl_response = curl_exec($curl);
-    if ($curl_response === false) {
-        $info = curl_getinfo($curl);
-        curl_close($curl);
-        die('error occured during curl exec. Additioanl info: ' . var_export($info));
-    } else {
-        die("Funciono");
-    }
-    curl_close($curl);
-    $decoded = json_decode($curl_response);
-    if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
-        die('error occured: ' . $decoded->response->errormessage);
-    }
-    echo 'response ok!';
-    var_export($decoded->response);
-    }
 ?>
     
 <script>
