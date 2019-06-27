9091b675e80f9dc9be005019b87861ede4d39ac7
incognita-es <51830325+incognita-es@users.noreply.github.com>
2019-06-27 22:01:53 +0200

    Update curl.php

--- curl.php
+++ curl.php
@@ -14,12 +14,12 @@ if (isset($decoded->RestResponse->status) && $decoded->RestResponse->status == '
     die('error occured: ' . $decoded->RestResponse->errormessage);
 }
 echo "JSON_DECODE:<br>";
+var_export($decoded->RestResponse);
 echo "Nombre: ".$decoded->RestResponse;
-//var_export($decoded->RestResponse);
-echo "Nombre1: ".$decoded->RestResponse->result;
-echo "Nombre2: ".$decoded->RestResponse->result->name;
-echo "Nombre3: ".$decoded->RestResponse->name;
-echo "Nombre4: ".$decoded->RestResponse->messages;
-echo "Nombre5: ".$decoded->messages;
-echo "Nombre6: ".$decoded->name;
+//echo "Nombre1: ".$decoded->RestResponse->result;
+//echo "Nombre2: ".$decoded->RestResponse->result->name;
+//echo "Nombre3: ".$decoded->RestResponse->name;
+//echo "Nombre4: ".$decoded->RestResponse->messages;
+//echo "Nombre5: ".$decoded->messages;
+//echo "Nombre6: ".$decoded->name;
 ?>
