2c9a6f835f8f4af404ddbbf24319b48148587bc2
incognita-es <51830325+incognita-es@users.noreply.github.com>
2019-06-27 21:03:16 +0200

    Update curl.php

--- curl.php
+++ curl.php
@@ -1,7 +1,7 @@
 <?php
 $entrada = $_REQUEST["entrada"];
 $cSession = curl_init(); 
-curl_setopt($cSession,CURLOPT_URL,"http://services.groupkt.com/country/search?text=$entrada");
+//curl_setopt($cSession,CURLOPT_URL,"http://services.groupkt.com/country/search?text=$entrada");
 curl_setopt($cSession,CURLOPT_URL,"http://services.groupkt.com/country/get/iso3code/$entrada");
 curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($cSession,CURLOPT_HEADER, false); 
