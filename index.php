e26d0b7d7d6f91ab139bbef141e90c7ea0bd065a
incognita-es <51830325+incognita-es@users.noreply.github.com>
2019-06-27 12:27:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,27 @@
 <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     
 <?php
-function curl($url) {
-  return $url;
-}
+    function buscaCurl($direccion){
+    //next example will recieve all messages for specific conversation
+    $service_url = $direccion;
+    $curl = curl_init($service_url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $curl_response = curl_exec($curl);
+    if ($curl_response === false) {
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+        die('error occured during curl exec. Additioanl info: ' . var_export($info));
+    } else {
+        die("Funciono");
+    }
+    curl_close($curl);
+    $decoded = json_decode($curl_response);
+    if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+        die('error occured: ' . $decoded->response->errormessage);
+    }
+    echo 'response ok!';
+    var_export($decoded->response);
+    }
 ?>
     
 <script>
@@ -34,7 +52,7 @@ function soloNombre(myString) {
 }
     
 function seleccion() {
-    var phpadd = <?php echo curl("http:sisisisi");?>;
+    var phpadd = <?php buscaCurl("http:sisisisi");?>;
     window.alert(phpadd);
     var x = document.getElementById("Listado").value;
     document.getElementById("Resultados").innerHTML = "Has seleccionado el c&oacute;digo: " + x;
