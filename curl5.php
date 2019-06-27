56dc60bc1d06375e4f2bdef971935758458ff44c
incognita-es <51830325+incognita-es@users.noreply.github.com>
2019-06-27 16:21:28 +0200

    Update curl5.php

--- curl5.php
+++ curl5.php
@@ -1,8 +1,11 @@
 <?php
+
+$entrada = $_REQUEST["entrada"];
+
 //step1
 $cSession = curl_init(); 
 //step2
-curl_setopt($cSession,CURLOPT_URL,"http://services.groupkt.com/country/search?text=ESP");
+curl_setopt($cSession,CURLOPT_URL,"http://services.groupkt.com/country/search?text=$entrada");
 curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($cSession,CURLOPT_HEADER, false); 
 //step3
